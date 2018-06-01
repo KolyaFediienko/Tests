e04d5e93e66fb9d8a88065367b8de42a49edd069
KolyaFediienko <kolya.fediienko@gmail.com>
2018-06-01 09:39:32 +0300

    new class

--- src/test/java/Pages/Pageforgit.java
+++ src/test/java/Pages/Pageforgit.java
@@ -1,8 +1,27 @@
 package Pages;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
 public class Pageforgit { public void clickPodatZayavky()
 {
-  
+
 }
 
+    @BeforeTest
+    protected WebDriver getDriver()
+    {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver_win32 (1)\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        PageFactory.initElements(driver,this);
+        return driver;
+    }
 }
