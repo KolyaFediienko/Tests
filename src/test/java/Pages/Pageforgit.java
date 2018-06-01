579a642b5c34831a154ba56846c05895e1db888d
KolyaFediienko <kolya.fediienko@gmail.com>
2018-06-01 09:51:41 +0300

    asdasd

--- src/test/java/Pages/Pageforgit.java
+++ src/test/java/Pages/Pageforgit.java
@@ -1,8 +1,15 @@
 package Pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Pageforgit { public void clickPodatZayavky()
 {
   
 }
+    @FindBy(xpath = "/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/table/tr/td[1]/div[2]/div[2]/div/div[3]/div/div/div/div/div/div[1]/div[2]/div[7]/div/div[1]/table/tbody/tr[2]/td/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr[4]/td/a")
+    private WebElement Messag1;
 
+    @FindBy(xpath = "/html/body/div[1]/div[4]/div/div[2]/div/form[2]/div/div[4]/button")
+    private WebElement passwordnew;
 }
